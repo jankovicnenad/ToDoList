f2c5c0f019cfd2249e77c34f5a4c12cf854caf09
Ninko <ninkomiletic@gmail.com>
2022-12-23 15:32:21 +0100

    Testt

--- src/main/java/com/example/ToDoList/DAO/ImageRepository.java
+++ src/main/java/com/example/ToDoList/DAO/ImageRepository.java
@@ -4,5 +4,5 @@ import com.example.ToDoList.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImageRepository extends JpaRepository<Image, Integer> {
-    
+    //Ninko komentarisao
 }
