963d0523ca8af33819b985af5d178b2f84355462
jankovicnenad <nenad.jankovic@dev.telnet.rs>
2023-01-11 15:06:24 +0100

    added new descriptions for open api

--- src/main/java/com/example/ToDoList/rest/PriorityRestController.java
+++ src/main/java/com/example/ToDoList/rest/PriorityRestController.java
@@ -3,6 +3,10 @@ package com.example.ToDoList.rest;
 import com.example.ToDoList.DTO.PriorityDtoResponse;
 import com.example.ToDoList.DTO.PriorityDtoRequest;
 import com.example.ToDoList.service.PriorityServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,9 +20,14 @@ public class PriorityRestController {
         {
             priorityService = theService;
         }
+
+    @Operation(summary = "Get all priorities from database")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Get all priorities from database", content = {@Content(mediaType = "application/json")})})
     @GetMapping("/priority")
     public List<PriorityDtoResponse> getAllPriority(){return priorityService.getAllPriority();}
 
+    @Operation(summary = "Get priority from database by specific ID")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Get priority from database by specific ID", content = {@Content(mediaType = "application/json")})})
     @GetMapping("/priority/{priorityId}")
     public PriorityDtoResponse findById(@PathVariable Long priorityId){
 
@@ -28,17 +37,25 @@ public class PriorityRestController {
         return priorityDtoResponse;
     }
 
+    @Operation(summary = "Insert priority to database")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Insert priority to database", content = {@Content(mediaType = "application/json")})})
     @PostMapping("/priority")
     public PriorityDtoResponse addPriority(@RequestBody PriorityDtoRequest thePriority){
         return priorityService.savePriority(thePriority);
 
     }
+
+    @Operation(summary = "Update priority in database")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Update priority in database", content = {@Content(mediaType = "application/json")})})
     @PutMapping("/priority")
     public String updatePriority(@RequestBody PriorityDtoRequest priorityDtoRequest){
         Long priorityId = priorityDtoRequest.getId();
         priorityService.updatePriority(priorityId, priorityDtoRequest);
         return "Updated priority with id - " +priorityId;
     }
+
+    @Operation(summary = "Delete priority from database")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Delete priority from database", content = {@Content(mediaType = "application/json")})})
     @DeleteMapping("/priority/{priorityId}")
     public String deletePriority(@PathVariable Long priorityId){
 
--- src/main/java/com/example/ToDoList/rest/StatusRestController.java
+++ src/main/java/com/example/ToDoList/rest/StatusRestController.java
@@ -3,6 +3,10 @@ package com.example.ToDoList.rest;
 import com.example.ToDoList.DTO.StatusDtoResponse;
 import com.example.ToDoList.DTO.StatusDtoRequest;
 import com.example.ToDoList.service.StatusServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,11 +21,15 @@ public class StatusRestController {
         statusService = theService;
     }
 
+    @Operation(summary = "Get all statuses from database")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "All statuses are extracted from database", content = {@Content(mediaType = "application/json")})})
     @GetMapping("/status")
     public List<StatusDtoResponse> getAllStatus() {
         return statusService.getAllStatus();
     }
 
+    @Operation(summary = "Get status from database with specific ID")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Fetched status with specific ID", content = {@Content(mediaType = "application/json")})})
     @GetMapping("/status/{statusId}")
     public StatusDtoResponse findById(@PathVariable Long statusId) {
 
@@ -31,12 +39,16 @@ public class StatusRestController {
         return statusDtoResponse;
     }
 
+    @Operation(summary = "Save status in database")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Saved status in the database", content = {@Content(mediaType = "application/json")})})
     @PostMapping("/status")
     public StatusDtoResponse saveStatus(@RequestBody StatusDtoRequest theStatus) {
         return statusService.save(theStatus);
 
     }
 
+    @Operation(summary = "Update status in database")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Fetched status with specific ID", content = {@Content(mediaType = "application/json")})})
     @PutMapping("/status")
     public String updateStatus(@RequestBody StatusDtoRequest statusDto) {
         Long statusId = statusDto.getId();
@@ -44,6 +56,8 @@ public class StatusRestController {
         return "Update status with id - " + statusId;
     }
 
+    @Operation(summary = "Delete status from database")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Deleted status from database", content = {@Content(mediaType = "application/json")})})
     @DeleteMapping("/status/{statusId}")
     public String deleteStatus(@PathVariable Long statusId) {
 
