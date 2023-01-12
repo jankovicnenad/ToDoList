b4d51966cbd1940850db94157feb554fcdb2b40b
jankovicnenad <nenad.jankovic@dev.telnet.rs>
2023-01-12 10:38:10 +0100

    checked code

--- src/main/java/com/example/ToDoList/rest/CommentRestController.java
+++ src/main/java/com/example/ToDoList/rest/CommentRestController.java
@@ -21,14 +21,14 @@ public class CommentRestController {
     {commentService = theComm;}
 
     @Operation(summary = "Get all comments from database")
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "All tasks are extracted from the database", content = {@Content(mediaType = "application/json")})})
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "All tasks are extracted from the database")})
     @GetMapping("/comments")
     public List<CommentDtoResponse> getAllComments(){
      return commentService.getAllComments();
     }
 
     @Operation(summary = "Get comment from database by specific ID")
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Got comment from database by specific ID", content = {@Content(mediaType = "application/json")})})
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Got comment from database by specific ID")})
     @GetMapping("/comments/{commentId}")
     public CommentDtoResponse findByid(@PathVariable Long commentId){
         CommentDtoResponse commentDtoResponse = commentService.findById(commentId);
@@ -39,7 +39,7 @@ public class CommentRestController {
 
 
     @Operation(summary = "Update comment in database")
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Updated comment in database", content = {@Content(mediaType = "application/json")})})
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Updated comment in database")})
     @PutMapping("/comments")
     public String updateComment(@RequestBody CommentDtoRequest commentDto){
         Long commentId = commentDto.getId();
@@ -48,7 +48,7 @@ public class CommentRestController {
     }
 
     @Operation(summary = "Save comment in database")
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Saved comment in database", content = {@Content(mediaType = "application/json")})})
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Saved comment in database")})
     @PostMapping("/comments")
     public CommentDtoResponse saveComment(@RequestBody CommentDtoRequest commDto)
     {
@@ -56,7 +56,7 @@ public class CommentRestController {
 
     }
     @Operation(summary = "Delete comment in database")
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Deleted comment in database", content = {@Content(mediaType = "application/json")})})
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Deleted comment in database")})
     @DeleteMapping("/comments/{commentId}")
     public String deleteComment(@PathVariable Long commentId){
 
